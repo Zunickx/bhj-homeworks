bbe67732507d12955870e89bb5925e4109bc93dd
Zunickx <Zunickx@gmail.com>
2023-08-10 00:15:17 +0300

    Create cart

--- document-structure/cart/task.js
+++ document-structure/cart/task.js
@@ -19,9 +19,13 @@ minusButtons.forEach((item, index) => {
 
 const productAdd = Array.from(querySelectorAll('.product__add')); // находим все кнопки "Добавить в корзину"
 console.log(productAdd);
+let cartProducts = document.querySelector('.cart__products'); // находим корзину с товарами, которые мы туда поместим
+const product = document.querySelectorAll('.products'); // находим все товары
+const productImage = document.querySelectorAll('.product__image'); //находим картинки товаров с классом product__image
 
 productAdd.forEach((item, index) => {
     item.addEventListener('click', (event) => {
-
+        const dataId = (product[index].getAttribute('data-id')); // находим атрибут data-id у каждого товара
+        const parents = productImage[index].closest('.product') // находим ближайшего родителя элемента на котором произошел клик, затем находим в этом элементе класс product__image, лежащий в переменной productImage, для того что бы получить изображение товара
     })
 })
